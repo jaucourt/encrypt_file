2f690ece41ffbc543d35463c97d10badcf6ad908
Phil Benson <phil.benson@defra.gov.uk>
2020-12-01 12:45:07 +0000

    Switch to using streams

--- decrypt.js
+++ decrypt.js
@@ -1,9 +1,11 @@
 import crypto from 'crypto'
 import fs from 'fs'
 const AES_KEY = 'snortingBuffaloWyldThingGruffalo'
+const IV = 'qwertyuiop123456'
 
-const iv = 'qwertyuiop123456'
-const d = crypto.createDecipheriv('aes-256-cbc', AES_KEY, iv)
-const eText = fs.readFileSync('./sample_text_encoded', { encoding: 'utf8' })
-const decrypted = `${d.update(eText, 'base64', 'utf8')}${d.final('utf8')}`
-fs.writeFileSync('./sample_text_decoded', decrypted, { encoding: 'utf8' })
+const input = fs.createReadStream('./sample_text_encoded');
+const output = fs.createWriteStream('./sample_text_decoded');
+const decipher = crypto.createDecipheriv('aes-256-cbc', AES_KEY, IV);
+input
+    .pipe(decipher)
+    .pipe(output)
--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import fs from 'fs'
 import crypto from 'crypto'
 
 const AES_KEY = 'snortingBuffaloWyldThingGruffalo'
+const IV = 'qwertyuiop123456'
 
 const encodeFile = (key, inputFileName, outputFileName) => {
-  const iv = 'qwertyuiop123456' // Buffer.from(crypto.randomBytes(16))
-  const cipher = crypto.createCipheriv('aes-256-cbc', AES_KEY, iv)
-  const text = fs.readFileSync(inputFileName, { encoding: 'utf8' })
-  const output = `${cipher.update(text, 'utf8', 'base64')}${cipher.final('base64')}`
-  fs.writeFileSync(outputFileName, output, { encoding: 'utf8' })
+  const cipher = crypto.createCipheriv(algorithm, key, IV);
+  const input = fs.createReadStream(inputFileName);
+  const output = fs.createWriteStream(outputFileName);
+  input 
+    .pipe(cipher)  
+    .pipe(output)
 }
 
 encodeFile(AES_KEY, './sample_text', './sample_text_encoded')
