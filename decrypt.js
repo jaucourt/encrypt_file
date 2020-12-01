d074c9b197c1c55ab16ec6cb104c97468be8053e
Phil Benson <phil.benson@defra.gov.uk>
2020-12-01 09:17:18 +0000

    fix decryption

--- decrypt.js
+++ decrypt.js
@@ -2,7 +2,8 @@ import crypto from 'crypto'
 import fs from 'fs'
 const AES_KEY = 'snortingBuffaloWyldThingGruffalo'
 
-const d = crypto.createDecipheriv('aes-256-cbc', AES_KEY, Buffer.from(crypto.randomBytes(16)));
-const eText = fs.readFileSync('./sample_text_encoded-new', { encoding: 'utf-8' })
-const decrypted = `${d.update(eText, 'base64', 'utf8')}${d.final('utf-8')}`
-fs.writeFileSync('./sample_text_decoded', decrypted, { encoding: 'utf-8' })
+const iv = 'qwertyuiop123456'
+const d = crypto.createDecipheriv('aes-256-cbc', AES_KEY, iv)
+const eText = fs.readFileSync('./sample_text_encoded', { encoding: 'utf8' })
+const decrypted = `${d.update(eText, 'base64', 'utf8')}${d.final('utf8')}`
+fs.writeFileSync('./sample_text_decoded', decrypted, { encoding: 'utf8' })
--- index.js
+++ index.js
@@ -3,26 +3,13 @@ import fs from 'fs'
 import crypto from 'crypto'
 
 const AES_KEY = 'snortingBuffaloWyldThingGruffalo'
-const BUFFER_ENCRYPTION = 'utf16le'
-
-const getKey = (aesKey, encryption) => {
-  if (aesKey.length < 32) {
-    throw new Error(`Key phrase isn't long enough, should be 32 characters long and it's only ${aesKey.length}`)
-  }
-  const keyBuffer = Buffer.from(aesKey, encryption);
-  const key = new Uint16Array(
-    keyBuffer.buffer,
-    keyBuffer.byteOffset,
-    keyBuffer.length / Uint16Array.BYTES_PER_ELEMENT);
-  return key.slice(0,32)
-}
 
 const encodeFile = (key, inputFileName, outputFileName) => {
-  const iv = Buffer.from(crypto.randomBytes(16))
+  const iv = 'qwertyuiop123456' // Buffer.from(crypto.randomBytes(16))
   const cipher = crypto.createCipheriv('aes-256-cbc', AES_KEY, iv)
-  const text = fs.readFileSync(inputFileName, { encoding: 'utf-8' })
-  const output = `${cipher.update(text, 'utf-8', 'base64')}${cipher.final('base64')}`
-  fs.writeFileSync(outputFileName, output, { encoding: 'utf-8' })
+  const text = fs.readFileSync(inputFileName, { encoding: 'utf8' })
+  const output = `${cipher.update(text, 'utf8', 'base64')}${cipher.final('base64')}`
+  fs.writeFileSync(outputFileName, output, { encoding: 'utf8' })
 }
 
-encodeFileNew(AES_KEY, './sample_text', './sample_text_encoded-new')
+encodeFile(AES_KEY, './sample_text', './sample_text_encoded')
