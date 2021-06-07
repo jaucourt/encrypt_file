5711b66026edda670036f49e3e934dfc825a698a
Phil Benson <phil.benson@defra.gov.uk>
2021-06-07 12:21:15 +0100

    Updated for decrypting files

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import openpgp from 'openpgp'
 import fs from 'fs'
 
 const passphrase = 'snortingBuffaloWyldThingGruffalo'
-const fileToEncode = 'sample_data'
+const fileToEncode = 'EAFF202105280002.json' // 'sample_data'
+const fileToDecode = 'fileToDecode.json'
 
 const generateKeys = async () => {
   const { privateKeyArmored, publicKeyArmored } = await openpgp.generateKey({
@@ -18,26 +19,28 @@ const privateKey = await openpgp.readKey({ armoredKey: fs.readFileSync('sample_p
 const publicKey = await openpgp.readKey({ armoredKey: fs.readFileSync('sample_public.key', {encoding: 'utf8'}) }) 
 
 const encodeFile = async () => {
-  const plainData = fs.readFileSync(`${fileToEncode}.csv`);
+  const plainData = fs.readFileSync(fileToEncode);
   const encrypted = await openpgp.encrypt({
     message: openpgp.Message.fromText(plainData),
     publicKeys: publicKey
   });
 
-  fs.writeFileSync(`${fileToEncode}_encoded`, encrypted);
-  console.log(`wrote file ${fileToEncode}_encoded`)
+  fs.writeFileSync(`${fileToEncode}.enc`, encrypted);
+  console.log(`wrote file ${fileToEncode}.enc`)
 }
 
 const decodeFile = async () => {
   await privateKey.decrypt(passphrase);
 
-  const encryptedData = fs.readFileSync(`${fileToEncode}_encoded`);
+  // const encryptedData = fs.readFileSync(`${fileToEncode}_encoded`);
+  const encryptedData = fs.readFileSync(`${fileToDecode}.json.enc`);
   const decrypted = await openpgp.decrypt({
     message: await openpgp.readMessage({ armoredMessage: encryptedData }),
     privateKeys: privateKey
   });
 
-  fs.writeFileSync(`${fileToEncode}_decoded.csv`, decrypted.data)
+  // fs.writeFileSync(`${fileToEncode}_decoded.csv`, decrypted.data)
+  fs.writeFileSync(`decoded_${fileToEncode}.json`, decrypted.data)
 }
 
 const thereAndBackAgain = async () => {
@@ -45,5 +48,6 @@ const thereAndBackAgain = async () => {
   await decodeFile()
 }
 
-thereAndBackAgain()
+// thereAndBackAgain()
 // encodeFile()
+decodeFile()
